fb5c6bc9483f01d28425e77dd2c837a9854f6777
robert-winn-ai <104718630+robert-winn-ai@users.noreply.github.com>
2023-06-20 15:32:06 +0300

    add app fort test

--- tests/e2e_zap.py
+++ tests/e2e_zap.py
@@ -12,19 +12,45 @@ proxies = {
     'https': 'http://127.0.0.1:8090',
 }
 
-# auth_dict = {'username': 'U_SECURITY_SCAN_4RUXQJK533', 'password': 'idui6sgt68oianb3iu4dy8o61vihwrwt'}
-#
-# login = requests.post(target_url + '/auth/security-test-login',
-#                       proxies=proxies, json=auth_dict, verify=False)
-#
-#
-# if login.status_code == 200:  # if login is successful
-#     auth_token = login.headers['Authorization']
-#     auth_header = {"Authorization": auth_token}
+auth_dict = {'username': 'U_SECURITY_SCAN_4RUXQJK533', 'password': 'idui6sgt68oianb3iu4dy8o61vihwrwt'}
+
+login = requests.post(target_url + '/auth/security-test-login',
+                      proxies=proxies, json=auth_dict, verify=False)
+
+
+if login.status_code == 200:  # if login is successful
+    auth_token = login.headers['Authorization']
+    auth_header = {"Authorization": auth_token}
+
+    # now lets run some operations
+    # GET Customer by ID
+
+    get_cust_id = requests.get(
+        target_url + 'management/#/meetings', proxies=proxies, headers=auth_header, verify=False)
+    if get_cust_id.status_code == 200:
+        print("Get Customer by ID Response")
+        print(get_cust_id.json())
+        print()
+
+    post = {'id': 2}
+    fetch_customer_post = requests.post(
+        target_url + '/fetch/customer', json=post, proxies=proxies, headers=auth_header, verify=False)
+    if fetch_customer_post.status_code == 200:
+        print("Fetch Customer POST Response")
+        print(fetch_customer_post.json())
+        print()
+
+    search = {'search': 'dleon'}
+    search_customer_username = requests.post(
+        target_url + '/search', json=search, proxies=proxies, headers=auth_header, verify=False)
+    if search_customer_username.status_code == 200:
+        print("Search Customer POST Response")
+        print(search_customer_username.json())
+        print()
 
 
 # ZAP Operations
-print("Zap Scan stat")
+
 zap = ZAP(proxies={'http': 'http://localhost:8090',
                    'https': 'http://localhost:8090'})
 
@@ -33,7 +59,7 @@ if 'Light' not in zap.ascan.scan_policy_names:
     zap.ascan.add_scan_policy(
         "Light", alertthreshold="Medium", attackstrength="Low")
 
-active_scan_id = zap.ascan.scan(target_url)
+active_scan_id = zap.ascan.scan(target_url, scanpolicyname='Light')
 
 print("active scan id: {0}".format(active_scan_id))
 
@@ -41,8 +67,8 @@ print("active scan id: {0}".format(active_scan_id))
 while int(zap.ascan.status(active_scan_id)) < 100:
     print("Current Status of ZAP Active Scan: {0}%".format(
         zap.ascan.status(active_scan_id)))
-    time.sleep(5)
-print("Zap Scan end")
+    time.sleep(10)
+
 now = datetime.datetime.now().strftime("%m/%d/%Y")
 alert_severity = 't;t;t;t'  # High;Medium;Low;Info
 # CWEID;#WASCID;Description;Other Info;Solution;Reference;Request Header;Response Header;Request Body;Response Body
