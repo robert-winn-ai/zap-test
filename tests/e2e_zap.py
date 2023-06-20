73bdd600201cceba8aaf501b0c31d6554b719b33
robert-winn-ai <104718630+robert-winn-ai@users.noreply.github.com>
2023-06-20 15:42:35 +0300

    add app fort test

--- tests/e2e_zap.py
+++ tests/e2e_zap.py
@@ -1,82 +1,27 @@
-import requests
-from zapv2 import ZAPv2 as ZAP
 import time
-import datetime
-from os import getcwd
-
-# Test Automation Part of the Script
-
-target_url = 'https://stg.winnai.dev'
-proxies = {
-    'http': 'http://127.0.0.1:8090',
-    'https': 'http://127.0.0.1:8090',
-}
-
-auth_dict = {'username': 'U_SECURITY_SCAN_4RUXQJK533', 'password': 'idui6sgt68oianb3iu4dy8o61vihwrwt'}
-
-login = requests.post(target_url + '/auth/security-test-login',
-                      proxies=proxies, json=auth_dict, verify=False)
+from zapv2 import ZAPv2
 
 
-if login.status_code == 200:  # if login is successful
-    auth_token = login.headers['Authorization']
-    auth_header = {"Authorization": auth_token}
+target = 'https://stg.winnai.dev/'
 
-    # now lets run some operations
-    # GET Customer by ID
+zap = ZAPv2(proxies={'http': 'http://127.0.0.1:8888', 'https': 'http://127.0.0.1:8888'})
 
-    get_cust_id = requests.get(
-        target_url + 'management/#/meetings', proxies=proxies, headers=auth_header, verify=False)
-    if get_cust_id.status_code == 200:
-        print("Get Customer by ID Response")
-        print(get_cust_id.json())
-        print()
+zap.context.import_context("/file/context")
 
-    post = {'id': 2}
-    fetch_customer_post = requests.post(
-        target_url + '/fetch/customer', json=post, proxies=proxies, headers=auth_header, verify=False)
-    if fetch_customer_post.status_code == 200:
-        print("Fetch Customer POST Response")
-        print(fetch_customer_post.json())
-        print()
+print ('Accessing target %s' % target)
+zap.urlopen(target)
+time.sleep(2)
 
-    search = {'search': 'dleon'}
-    search_customer_username = requests.post(
-        target_url + '/search', json=search, proxies=proxies, headers=auth_header, verify=False)
-    if search_customer_username.status_code == 200:
-        print("Search Customer POST Response")
-        print(search_customer_username.json())
-        print()
+print ('Spidering target %s' % target)
+scanid = zap.spider.scan(target)
 
+time.sleep(2)
 
-# ZAP Operations
+print ('Spider completed')
 
-zap = ZAP(proxies={'http': 'http://localhost:8090',
-                   'https': 'http://localhost:8090'})
+time.sleep(5)
 
-if 'Light' not in zap.ascan.scan_policy_names:
-    print("Adding scan policies")
-    zap.ascan.add_scan_policy(
-        "Light", alertthreshold="Medium", attackstrength="Low")
+print ('Scanning target %s' % target)
+scanid = zap.ascan.scan(target)
 
-active_scan_id = zap.ascan.scan(target_url, scanpolicyname='Light')
-
-print("active scan id: {0}".format(active_scan_id))
-
-# now we can start monitoring the spider's status
-while int(zap.ascan.status(active_scan_id)) < 100:
-    print("Current Status of ZAP Active Scan: {0}%".format(
-        zap.ascan.status(active_scan_id)))
-    time.sleep(10)
-
-now = datetime.datetime.now().strftime("%m/%d/%Y")
-alert_severity = 't;t;t;t'  # High;Medium;Low;Info
-# CWEID;#WASCID;Description;Other Info;Solution;Reference;Request Header;Response Header;Request Body;Response Body
-alert_details = 't;t;t;t;t;t;f;f;f;f'
-source_info = 'Vulnerability Report for Winn.ai;Abhay Bhargav;API Team;{};{};v1;v1;API Scan Report'.format(
-    now, now)
-path = getcwd() + "/zap-report.json"
-zap.exportreport.generate(path, "json", sourcedetails=source_info,
-                          alertseverity=alert_severity, alertdetails=alert_details, scanid=active_scan_id)
-
-zap.core.shutdown()
+print ('Scan completed')
