266ecb6d30015359ddfbb832047190fb217f511e
robert-winn-ai <104718630+robert-winn-ai@users.noreply.github.com>
2023-06-20 15:29:01 +0300

    add app fort test

--- tests/e2e_zap.py
+++ tests/e2e_zap.py
@@ -11,49 +11,38 @@ proxies = {
     'http': 'http://127.0.0.1:8090',
     'https': 'http://127.0.0.1:8090',
 }
-# ZAP Operations
-print("Zap Scan stat")
-zap = ZAP(proxies={'http': 'http://localhost:8090',
-                    'https': 'http://localhost:8090'})
-print ('Active Scanning target {}'.format(target_url))
-scanid = zap.ascan.scan(target_url)
-while (int(zap.ascan.status(scanid)) < 100):
-    # Loop until the scanner has finished
-    print ('Scan progress %: {}'.format(zap.ascan.status(scanid)))
-    time.sleep(5)
-print ('Active Scan completed')
 
 # auth_dict = {'username': 'U_SECURITY_SCAN_4RUXQJK533', 'password': 'idui6sgt68oianb3iu4dy8o61vihwrwt'}
 #
 # login = requests.post(target_url + '/auth/security-test-login',
 #                       proxies=proxies, json=auth_dict, verify=False)
-
-
+#
+#
 # if login.status_code == 200:  # if login is successful
 #     auth_token = login.headers['Authorization']
 #     auth_header = {"Authorization": auth_token}
-#
-#
-# # ZAP Operations
-# print("Zap Scan stat")
-# zap = ZAP(proxies={'http': 'http://localhost:8090',
-#                    'https': 'http://localhost:8090'})
-#
-# if 'Light' not in zap.ascan.scan_policy_names:
-#     print("Adding scan policies")
-#     zap.ascan.add_scan_policy(
-#         "Light", alertthreshold="Medium", attackstrength="Low")
-#
-# active_scan_id = zap.ascan.scan(target_url, scanpolicyname='Light')
-#
-# print("active scan id: {0}".format(active_scan_id))
-#
-# # now we can start monitoring the spider's status
-# while int(zap.ascan.status(active_scan_id)) < 360:
-#     print("Current Status of ZAP Active Scan: {0}%".format(
-#         zap.ascan.status(active_scan_id)))
-#     time.sleep(10)
-#print("Zap Scan end")
+
+
+# ZAP Operations
+print("Zap Scan stat")
+zap = ZAP(proxies={'http': 'http://localhost:8090',
+                   'https': 'http://localhost:8090'})
+
+if 'Light' not in zap.ascan.scan_policy_names:
+    print("Adding scan policies")
+    zap.ascan.add_scan_policy(
+        "Light", alertthreshold="Medium", attackstrength="Low")
+
+active_scan_id = zap.ascan.scan(target_url)
+
+print("active scan id: {0}".format(active_scan_id))
+
+# now we can start monitoring the spider's status
+while int(zap.ascan.status(active_scan_id)) < 100:
+    print("Current Status of ZAP Active Scan: {0}%".format(
+        zap.ascan.status(active_scan_id)))
+    time.sleep(5)
+print("Zap Scan end")
 now = datetime.datetime.now().strftime("%m/%d/%Y")
 alert_severity = 't;t;t;t'  # High;Medium;Low;Info
 # CWEID;#WASCID;Description;Other Info;Solution;Reference;Request Header;Response Header;Request Body;Response Body
